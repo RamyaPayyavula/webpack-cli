c6c063c0074934beaf7ed71b5c51c63f92147002
Nitin Kumar <snitin315@gmail.com>
2020-08-06 16:26:54 +0530

    tests: loader's error regression test for #1581

---
+++ test/loader/error-test/loader-error.test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// eslint-disable-next-line node/no-unpublished-require
+const { run } = require('../../utils/test-utils');
+
+describe('loader error regression test for #1581', () => {
+    it(`should not ignore loader's error produce a failing build`, () => {
+        const { stderr, stdout } = run(__dirname, [], false);
+        expect(stderr).toBeFalsy();
+        expect(stdout).toContain('[built] [failed] [1 error]');
+        expect(stdout).toContain(`Cannot assign to 'foobar' because it is a constant`);
+    });
+});
